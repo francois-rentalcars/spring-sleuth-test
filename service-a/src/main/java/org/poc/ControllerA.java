73a32b7c368a84dcc84a5d31cd563104ac15ea7e
Francois Aubertin <FrancoisAubertin@rentalcars.com>
2018-10-10 09:58:24 +0100

    Implementing Sleuth POC

--- service-a/src/main/java/org/poc/ControllerA.java
+++ service-a/src/main/java/org/poc/ControllerA.java
@@ -1,15 +1,43 @@
 package org.poc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ControllerA {
 
+    private static Logger log = LoggerFactory.getLogger(ControllerA.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient webClient;
+
     @GetMapping("/foo")
     public String foo() {
-        return new RestTemplate().getForObject("http://localhost:8181/bar", String.class);
+        log.info("Handling request for /foo");
+        String response = restTemplate.getForObject("http://localhost:8181/bar", String.class);
+        log.info("Returning response for /foo");
+        return response;
+    }
+
+    @GetMapping("/foo-async")
+    public Mono<String> fooAsync() {
+        log.info("Handling request for /foo-async");
+        return webClient.get()
+                .uri("http://localhost:8181/bar")
+                .exchange()
+                .flatMap(response -> {
+                    log.info("Returning response for /foo-async");
+                    return response.bodyToMono(String.class);
+                });
     }
 
 }
--- service-b/src/main/java/org/poc/ControllerB.java
+++ service-b/src/main/java/org/poc/ControllerB.java
@@ -1,13 +1,18 @@
 package org.poc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerB {
 
+    private static Logger log = LoggerFactory.getLogger(ControllerB.class);
+
     @GetMapping("/bar")
     public String bar() {
+        log.info("Handling request for /bar");
         return "value";
     }
 
